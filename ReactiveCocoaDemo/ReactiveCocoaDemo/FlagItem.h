ed23a007312ba5ae52126c0fb5266bdf7624647c
信息科技部 <itd@xinxikejibudeiMac.local>
2019-01-07 08:53:39 +0800

    ReactiveCocoa使用

---
+++ ReactiveCocoaDemo/ReactiveCocoaDemo/FlagItem.h
@@ -0,0 +1,17 @@
+//
+//  FlagItem.h
+//  ReactiveCocoaDemo
+//
+//  Created by 信息科技部 on 2019/1/4.
+//  Copyright © 2019 信息科技部. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FlagItem : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ReactiveCocoaDemo/ReactiveCocoaDemo/FlagItem.m
@@ -0,0 +1,13 @@
+//
+//  FlagItem.m
+//  ReactiveCocoaDemo
+//
+//  Created by 信息科技部 on 2019/1/4.
+//  Copyright © 2019 信息科技部. All rights reserved.
+//
+
+#import "FlagItem.h"
+
+@implementation FlagItem
+
+@end
